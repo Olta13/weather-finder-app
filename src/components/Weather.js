97738222a0d14cffce131cce5162da307827f2fd
Olta <olta.petriti13@gmail.com>
2019-05-15 16:36:43 +0200

    Add units to weather info

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -11,13 +11,13 @@ const Weather = props => (
 
         { 
             props.temperature && <p className="weather__key">Temperature: 
-                <span className="weather__value"> {props.temperature}</span>
+                <span className="weather__value"> {props.temperature} &deg;C</span>
             </p>
         }
 
         { 
             props.humidity && <p className="weather__key">Humidity: 
-               <span className="weather__value">  {props.humidity}</span> 
+               <span className="weather__value">  {props.humidity} %</span> 
            </p> 
         } 
 
